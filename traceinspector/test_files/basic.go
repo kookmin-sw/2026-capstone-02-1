294b270b3ac3038ddc5d386da7bec4cf93b36a2f
Dashadower <tttllshin@gmail.com>
2026-04-20 17:14:20 +0900

    Change some test code to support simple inequality

--- traceinspector/test_files/basic.go
+++ traceinspector/test_files/basic.go
@@ -9,6 +9,7 @@ func add1(a int) int {
 }
 
 func composite(a int, b int, c int) bool {
+	g := (a-b)-(0-1) <= 2
 	return 5*a+4/2-2+(2+a)*b+11+add1(a) == 1 //5a + 2 - 2 + (2 + a)b + 11 = 1 -> 5a + (2 + a)b + 10
 }
 
